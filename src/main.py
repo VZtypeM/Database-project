71e4c4defbd5fc0c77a21e8f468295e2676c6472
Markus Risa Vesetrud <markusrv@stud.ntnu.no>
2022-03-19 09:56:49 +0100

    find roastery id from roastery and coffee name

--- src/main.py
+++ src/main.py
@@ -5,8 +5,37 @@ from datetime import date
 database_name = "test.db"
 
 
-def add_coffee_taste(email, coffee_name, roastery_id, points, notes):
+def find_roastery_id_from_names(coffee_name: str, roastery_name: str) -> str | None:
+    connection = sqlite3.connect(database_name)
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        SELECT DISTINCT RoasteryID
+        FROM Roastery JOIN CoffeeTastes USING (RoasteryID)
+        WHERE CoffeeName = ? AND Roastery.name = ?;
+        """,
+        (coffee_name, roastery_name),
+    )
+    result = cursor.fetchall()
+    connection.close()
+
+    if len(result) > 1:
+        print("Roastery.name and CoffeeName are not unique in the database!")
+        return None
+    if len(result) == 0:
+        print("No matching Roastery.name and CoffeeName in the database!")
+        return None
+
+    # Return roastery id
+    return result[0][0]
+
+
+def add_coffee_taste(
+    email: str, coffee_name: str, roastery_name: str, points: int, notes: str
+) -> None:
     date_today = date.today()
+    roastery_id = find_roastery_id_from_names(coffee_name, roastery_name)
 
     connection = sqlite3.connect(database_name)
     cursor = connection.cursor()
@@ -35,7 +64,13 @@ def main():
     selected = pick.pick(options, title)
     print(selected)
     if selected[0] == "add coffee taste":
-        add_coffee_taste("ola.nordman@gmail.com", "Vinterkaffe 2022", 2, 7, "Splendid")
+        add_coffee_taste(
+            "ola.nordman@gmail.com",
+            "Vinterkaffe 2022",
+            "Trondheim brewery Jacobsen & Svart",
+            7,
+            "Splendid",
+        )
 
 
 if __name__ == "__main__":
