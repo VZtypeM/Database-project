c92cde3d06fcef8bf1b06367db01ad477b34de7f
Markus Risa Vesetrud <markusrv@stud.ntnu.no>
2022-03-25 16:00:08 +0100

    clean up

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from pick import pick
 from tabulate import tabulate
 import sqlite3
-from add_coffee_taste import add_coffee_taste
-from add_coffee_taste import find_roastery_id_from_names
+import add_coffee_taste
 from datetime import datetime
 from general_search import general_search
 
@@ -45,20 +44,8 @@ def log_out(database_name: str):
     user_email = None
 
 
-# def read_query(connection, query):
-#     cursor = connection.cursor()
-#     output = None
-
-#     try:
-#         cursor.execute(query)
-#         output = cursor.fetchall()
-#         return output
-#     except Error as err:
-#         print(f"Error: '{err}'")
-
-
 # User story 1
-def add_coffee_taste_handler(database_name: str):
+def add_coffee_taste_handler(database_name: str) -> None:
     # Checks that the user is logged in
     # Asks the user to enter the name of the roastery and coffee
     # the number of points and any notes
@@ -71,10 +58,13 @@ def add_coffee_taste_handler(database_name: str):
         "Enter the name of the roastery where your coffee was roasted: "
     )
 
-    roastery_id = find_roastery_id_from_names(database_name, coffee_name, roastery_name)
+    roastery_id = add_coffee_taste.find_roastery_id_from_names(
+        database_name, coffee_name, roastery_name
+    )
     if roastery_id is None:
         print("Database not modified")
         return
+
     print(type(roastery_id))
     print("Entering points ...")
     _, points = pick(
@@ -85,7 +75,7 @@ def add_coffee_taste_handler(database_name: str):
 
     notes = input("Write down any notes about the coffee: ")
 
-    add_coffee_taste(
+    add_coffee_taste.add_coffee_taste(
         database_name=database_name,
         email=user_email,
         coffee_name=coffee_name,
@@ -96,7 +86,7 @@ def add_coffee_taste_handler(database_name: str):
 
 
 # User story 2
-def print_users_with_coffee_tastes(database_name: str):
+def print_users_with_coffee_tastes(database_name: str) -> None:
     # Searches the database for the number of unique coffees each user has tasted
     # Prints the full name of all users who have drunk at least 1 coffee,
     # and the number of coffees they have drunk
@@ -130,7 +120,7 @@ def print_users_with_coffee_tastes(database_name: str):
 
 
 # User story 3
-def show_coffee_value(database_name: str):
+def show_coffee_value(database_name: str) -> None:
     # Calculates the coffee value as the average rating of the coffee divided by the price
     # Displays the coffee and roastery name, the price, and this value for all coffees that have been rated at least once
     connection = sqlite3.connect(database_name)
@@ -179,50 +169,22 @@ def show_coffee_value(database_name: str):
     )
 
 
-# generic input sanitizer
-# def choice_parser(prompt, type_=None, min_=None, max_=None, range_=None):
-#     if min_ is not None and max_ is not None and max_ < min_:
-#         raise ValueError("min_ must be less than or equal to max_.")
-#     while True:
-#         try:
-#             ui = type_(ui)
-#         except ValueError:
-#             print("Input type must be {0}.".format(type_.__name__))
-#             continue
-#         if max_ is not None and ui > max_:
-#             print("Input must be less than or equal to {0}.".format(max_))
-#         elif min_ is not None and ui < min_:
-#             print("Input must be greater than or equal to {0}.".format(min_))
-#         elif range_ is not None and ui not in range_:
-#             if isinstance(range_, range):
-#                 template = "Input must be between {0.start} and {0.stop}."
-#                 print(template.format(range_))
-#             else:
-#                 template = "Input must be {0}."
-#                 if len(range_) == 1:
-#                     print(template.format(*range_))
-#                 else:
-#                     expected = " or ".join(
-#                         (", ".join(str(x) for x in range_[:-1]), str(range_[-1]))
-#                     )
-#                     print(template.format(expected))
-#         else:
-#             return ui
+def reset_database(database_name: str) -> None:
+    with open("src/coffee.sql", "r") as sql_file:
+        sql_script = sql_file.read()
+
+    connection = sqlite3.connect(database_name)
+    cursor = connection.cursor()
+    cursor.executescript(sql_script)
+    connection.commit()
+    connection.close()
 
 
 def main():
     database_name = "src/coffee.db"
 
-    # Uncomment the following lines to reset the database by running the coffee.sql file
-
-    # with open('src/coffee.sql', 'r') as sql_file:
-    #     sql_script = sql_file.read()
-
-    # connection = sqlite3.connect(database_name)
-    # cursor = connection.cursor()
-    # cursor.executescript(sql_script)
-    # connection.commit()
-    # connection.close()
+    # Uncomment the following line to reset the database by running the coffee.sql file
+    # reset_database(database_name)
 
     # This object describes the different options the user has to search or change the database
     # The "handler" key references one of the functions above
@@ -252,6 +214,9 @@ def main():
             "confirmation_message": "Printing coffee values sorted on average_rating/price\n",
             "handler": show_coffee_value,
         },
+        # These commente out options correspond to the spesific queries to solve user story 4 and 5
+        # User story 4 and 5 are now solved by the general_search
+        # Our previous way of doing it can be found in "not_used.py"
         # {
         #     "label": "Search coffees based on descriptions",
         #     "confirmation_message": "Coffee description search selected\n",
